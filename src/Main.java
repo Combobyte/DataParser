210d4407150a344b86bda5cf02be942e1136f5db
vsreedhar337 <varun.sreedhar1@gmail.com>
2019-03-04 10:06:20 -0800

    Added readFileAsString

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,9 @@
  */
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Utils.readFileAsString("C:/Users/vsreedhar337/Desktop/2016_Presidential_Results.csv"));
+
+    }
 }
