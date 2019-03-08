ae5fc9582a31811e8002dfc5b60c853815d3f55a
vsreedhar337 <varun.sreedhar1@gmail.com>
2019-03-08 10:20:41 -0800

    Added organization Classes

---
+++ src/County.java
@@ -0,0 +1,8 @@
+public class County {
+
+    private String name;
+    private int fips;
+    private Election2016 election;
+    private Education2016 education;
+    private Employment2016 employment;
+}
---
+++ src/State.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class State {
+
+    private String name;
+    private ArrayList<County> counties;
+}
