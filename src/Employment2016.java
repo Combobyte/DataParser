2fff05f4c1a6025382a6e84bf72c89862024da91
Varun <vsreedhar337@student.fuhsd.org>
2019-03-10 01:43:27 -0800

    finished Election2016 class

--- src/Employment2016.java
+++ src/Employment2016.java
@@ -4,4 +4,44 @@ public class Employment2016 {
     private int employedLaborForce;
     private int unemployedLaborForce;
     private double percentEmployed;
+
+    public Employment2016(int total, int employed, int unemployed, double percent){
+        totalLaborForce = total;
+        employedLaborForce = employed;
+        unemployedLaborForce = unemployed;
+        percentEmployed = percent;
+
+    }
+
+    public int getTotalLaborForce() {
+        return totalLaborForce;
+    }
+
+    public void setTotalLaborForce(int totalLaborForce) {
+        this.totalLaborForce = totalLaborForce;
+    }
+
+    public int getEmployedLaborForce() {
+        return employedLaborForce;
+    }
+
+    public void setEmployedLaborForce(int employedLaborForce) {
+        this.employedLaborForce = employedLaborForce;
+    }
+
+    public int getUnemployedLaborForce() {
+        return unemployedLaborForce;
+    }
+
+    public void setUnemployedLaborForce(int unemployedLaborForce) {
+        this.unemployedLaborForce = unemployedLaborForce;
+    }
+
+    public double getPercentEmployed() {
+        return percentEmployed;
+    }
+
+    public void setPercentEmployed(double percentEmployed) {
+        this.percentEmployed = percentEmployed;
+    }
 }
